f68d88be15937e5daed8dc8d4f4c70d19eb30477
erhanikinci <erhanikinci61@gmail.com>
2025-03-12 23:42:29 +0300

    New account UI

--- app/accounts.tsx
+++ app/accounts.tsx
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import AccountListItem from "../components/AccountListItem";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import React, { useState } from "react";
 import AccountsList from "@/components/AccountsList";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function AccountsScreen() {
+
+  const [name, setName] = useState('')
+  const [cap, setCap] = useState('')
+  const [tap, setTap] = useState('')
+
+  const createAccount = () => {
+    console.warn('Create account')
+  }
   return (
     <View style={{ gap: 5, padding: 5 }}>
       <View style={styles.header}>
@@ -13,14 +21,32 @@ export default function AccountsScreen() {
       </View>
 
       <AccountsList />
+
+      <View style={styles.inputRow}>
+        <TextInput placeholder="Name" style={styles.input} value={name} onChangeText={setName} />
+        <TextInput placeholder="CAP %" style={styles.input} value={cap} onChangeText={setCap} />
+        <TextInput placeholder="TAP %" style={styles.input} value={tap} onChangeText={setTap} />
+        {/* <AntDesign name="check" size={20} color="green" /> */}
+      </View>
+
+      <Button title="Add account" onPress={createAccount} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: { 
-    flexDirection: "row", 
-    justifyContent: "space-between" ,
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: 10,
   },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  input: {
+    flex: 1,
+  }
 });
